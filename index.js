2f363a278a2c102d157c706a385277b3d02da537
Alisson Andrade <alisson14101998@gmail.com>
2021-10-13 23:58:08 -0300

    Inserção de token e JWT na aplicação

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+
+const JWTsecret = "maoehihisecretpassword";
 
 app.use(cors());
-
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
@@ -27,6 +29,20 @@ var DB = {
             title: "Minecraft",
             year: 2009,
             price: 100
+        }
+    ],
+    users: [
+        {
+            id: 1,
+            name: "Alisson Andrade",
+            email: "alisson@outlook.com",
+            password: "alisson"
+        },
+        {
+            id: 23,
+            name: "Ualisson Andrade",
+            email: "ualisson@outlook.com",
+            password: "ualisson"
         }
     ]
 }
@@ -109,6 +125,38 @@ app.put("/game/:id", (req, res) => {
     }
 });
 
+app.post("/auth", (req, res) => {
+    var { email, password } = req.body;
+
+    if(email != undefined){
+        var user = DB.users.find(u => u.email == email);
+
+        if(user != undefined){
+            if(user.password == password){
+                jwt.sign({id: user.id, email: user.email},JWTsecret,{expiresIn:'4h'},(err, token) => {
+                    if(err){
+                        res.status(400);
+                        res.json({err: "Falha interna"});
+                    }else{
+                        res.status(200);
+                        res.json({token: token});
+                    }
+                });
+            }else{
+                res.status(401);
+                res.json({err: "Credenciais inválidas"});
+            }
+
+        }else{
+            res.status(404);
+            res.json({ err: "Email enviado não existe na base de dados"});
+        }
+    }else{
+        res.status(400);
+        res.json({ err: "E-mail enviado é inválido"});
+    }
+
+});
 
 app.listen(8080, () => {
     console.log("Api rodando");
